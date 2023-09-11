6624b14322cc6d64865cbc136dbfa90d3c014992
Gerda <gerdusyte522@gmail.com>
2023-09-11 13:06:43 +0200

    Created a separate json file

--- scripts.js
+++ scripts.js
@@ -1,44 +1,7 @@
 const json_data = "data";
-const API_URL = `https://wu-e23a-fetch-arrays-default-rtdb.europe-west1.firebasedatabase.app/jylland-${json_data}.json`;
+const API_URL = `datausers.json`;
 
-const defaultData = [
-    {
-      "name": "Alice Johnson",
-      "age": 28,
-      "city": "Los Angeles",
-      "email": "alice@example.com",
-      "is_subscribed": true,
-      "hobbies": ["reading", "yoga", "traveling"],
-      "address": {
-        "street": "456 Elm Street",
-        "zipcode": "90001"
-      }
-    },
-    {
-      "name": "Bob Smith",
-      "age": 35,
-      "city": "Chicago",
-      "email": "bob@example.com",
-      "is_subscribed": false,
-      "hobbies": ["gardening", "painting", "running"],
-      "address": {
-        "street": "789 Oak Avenue",
-        "zipcode": "60601"
-      }
-    },
-    {
-      "name": "Eva Davis",
-      "age": 22,
-      "city": "San Francisco",
-      "email": "eva@example.com",
-      "is_subscribed": true,
-      "hobbies": ["swimming", "cooking", "photography"],
-      "address": {
-        "street": "101 Pine Road",
-        "zipcode": "94101"
-      }
-    }
-  ]
+
 
 /* fetch(API_URL, {
     headers: {Accept: "application/json"},
